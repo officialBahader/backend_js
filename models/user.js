f5ac90f6cbcaf1d3f5b0b40197741ea5e79b0f2f
Bahader <bahader@gmail.com>
2023-12-24 02:46:45 +0530

    local

--- models/user.js
+++ models/user.js
@@ -1,10 +1,34 @@
 const mongoose = require('mongoose');
-
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
-  username: String,
-  email: String,
-  // Add other fields as needed
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    validate: {
+      validator: function (v) {
+        return /^\S+@\S+\.\S+$/.test(v);
+      },
+      message: props => `${props.value} is not a valid email address!`,
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 const User = mongoose.model('User', userSchema);
--- routes/users.js
+++ routes/users.js
@@ -1,52 +1,149 @@
 var express = require('express');
 var router = express.Router();
 var User = require('../models/user');
+var bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+// Token verification middleware
+// function verifyToken(req, res, next) {
+//   const token = req.headers.authorization;
+
+//   if (!token) {
+//     return res.status(401).json({ message: 'Unauthorized: No token provided' });
+//   }
+// console.log(token);
+//   jwt.verify(token, "1234567890", (err, decoded) => {
+//     if (err) {
+//       return res.status(401).json({ message: 'Unauthorized: Invalid token' });
+//     }
+
+//     req.userId = decoded.userId;
+//     next();
+//   });
+// }
+function verifyToken(req, res, next) {
+  
+  if (req.headers['authorization']) {
+      try {
+          let authorization = req.headers['authorization'].split(' ');
+          console.log(authorization[1]);
+          if (authorization[0] !== 'Bearer') {
+              return res.status(401).send('invalid request'); //invalid request
+          } else {
+              jwt.verify(authorization[1], "1234567890", (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ message: 'Unauthorized: Invalid token' });
+    }
+
+    req.userId = decoded.userId;
+    next();
+  });
+          }
+      } catch (err) {
+        return res.status(403).send({
+          message:'invalid request'
+        });}
+  } else {
+      return res.status(401).send({
+        message:'invalid request'
+      });
+  }
+}
+// GET user profile based on token (requires authentication)
+router.post('/profile', verifyToken, async function(req, res, next) {
+  try {
+    const newUser = await User.findById(req.userId);
+    res.json({ user: newUser });
+  } catch (error) {
+    next(error);
+  }
+});
 
 // GET all users
-router.get('/', async function(req, res, next) {
+router.post('/all', async function(req, res, next) {
   try {
     const users = await User.find();
-    res.json(users);
+    res.json({users:users});
   } catch (error) {
     next(error);
   }
 });
 
-// GET a specific user by ID
-router.get('/:id', async function(req, res, next) {
+// POST create a new userconst bcrypt = require('bcrypt');
+const saltRounds = 10; 
+router.post('/register', async function(req, res, next) {
   try {
-    const user = await User.findById(req.params.id);
-    res.json(user);
+    // Hash the password before saving it
+    const hashedPassword = await bcrypt.hash(req.body.password,
+       saltRounds);
+
+    // Create a new user with the hashed password
+    const newUser = await User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
+      age: req.body.age, 
+    });
+
+    const token = jwt.sign({ userId: newUser._id },
+       '1234567890');
+
+    res.json({ user: newUser, accessToken:token }); 
+   } catch (error) {
+    next(error);
+  }
+});
+
+
+// POST login (without JWT token)
+router.post('/login', async function(req, res, next) {
+  const { email, password } = req.body;
+
+  try {
+    // Find the user by email
+    const user = await User.findOne({ email });
+
+    // If the user is not found
+    if (!user) {
+      return res.status(401).json({ message: 'Invalid email' });
+    }
+
+    // Compare the provided password with the hashed password in the database
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    // If the password does not match
+    if (!passwordMatch) {
+      return res.status(401).json({ message: 'Invalid password' });
+    }
+
+    // Password matches, create an access token
+    const accessToken = jwt.sign({ userId: user._id }, "1234567890"); 
+
+    // You can send the access token in the response or handle it as needed
+    res.json({user:user, accessToken:accessToken });
   } catch (error) {
     next(error);
   }
 });
 
-// POST create a new user
-router.post('/', async function(req, res, next) {
+
+// PUT update a user profile by ID
+router.post('/updateProfile', verifyToken, async function(req, res, next) {
   try {
-    const newUser = await User.create(req.body);
-    res.json(newUser);
+    const updatedUser = await User.findByIdAndUpdate(req.userId, req.body, { new: true });
+    res.json({user:updatedUser});
   } catch (error) {
     next(error);
   }
 });
 
-// PUT update a user by ID
-router.put('/:id', async function(req, res, next) {
+// DELETE user account by ID
+router.post('/deleteAccount', verifyToken, async function(req, res, next) {
   try {
-    const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
-    res.json(updatedUser);
-  } catch (error) {
-    next(error);
-  }
-});
-
-// DELETE a user by ID
-router.delete('/:id', async function(req, res, next) {
-  try {
-    await User.findByIdAndDelete(req.params.id);
-    res.json({ message: 'User deleted successfully' });
+    const updatedUser = await User.findByIdAndDelete(req.userId);
+    res.json({user:updatedUser,
+       message: 'User account deleted successfully',
+       });
   } catch (error) {
     next(error);
   }
